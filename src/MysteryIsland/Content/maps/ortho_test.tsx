fdd7f36dfd08e1cfd206919d989a1c69210d0228
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-10-21 00:52:09 +0530

    Get a map working

---
+++ src/MysteryIsland/Content/maps/ortho_test.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ortho_test" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="./tileset/map_tiles.png" width="1920" height="1080"/>
+</tileset>
--- src/MysteryIsland/Game1.cs
+++ src/MysteryIsland/Game1.cs
@@ -2,6 +2,15 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+
+// To get the camera
+using MonoGame.Extended;
+// Then you need a viewport adapter
+using MonoGame.Extended.ViewportAdapters;
+
+
 namespace MysteryIsland
 {
     public class Game1 : Game
@@ -9,16 +18,28 @@ namespace MysteryIsland
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // The camera object
+        private OrthographicCamera camera;
+        private TiledMap map;
+        private TiledMapRenderer mapRenderer;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+            // _graphics.IsFullScreen = true;
+
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        {   
+            // Initialize logic goes before the base.Initialize
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 832;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -27,7 +48,13 @@ namespace MysteryIsland
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            // Load the map
+            map = Content.Load<TiledMap>("maps/ortho");
+            // Create the map renderer
+            mapRenderer = new TiledMapRenderer(GraphicsDevice, map);
+            // If you decided to use the camere, then you could also initialize it here like this
+            var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 1280, 832);
+            camera = new OrthographicCamera(viewportadapter);
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,14 +64,29 @@ namespace MysteryIsland
 
             // TODO: Add your update logic here
 
+            // Update the map renderer
+            mapRenderer.Update(gameTime);
+            // If you used the camera then we update it aswell
+            camera.LookAt(new Vector2(640, 416));
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+
+
+            // Transform matrix is only needed if you have a camera
+            // Setting the sampler state to `new SamplerState { Filter = TextureFilter.Point }` will reduce gaps and odd artifacts when using animated tiles
+            _spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(), samplerState: new SamplerState { Filter = TextureFilter.Point });
+
+            // Then we will render the map
+            mapRenderer.Draw(camera.GetViewMatrix());
+
+            // End the sprite batch
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
