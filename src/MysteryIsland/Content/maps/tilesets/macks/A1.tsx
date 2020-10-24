c4e787beda3e54c7d1c257c40515051e9ee58182
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-10-24 17:03:11 +0530

    Add macks & pokemon tileset + a experimental map

---
+++ src/MysteryIsland/Content/maps/tilesets/macks/A1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="A1" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="tilea1.png" width="512" height="384"/>
+</tileset>
---
+++ src/MysteryIsland/Content/maps/tilesets/macks/A4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="A4" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="tilea4.png" width="512" height="480"/>
+</tileset>
