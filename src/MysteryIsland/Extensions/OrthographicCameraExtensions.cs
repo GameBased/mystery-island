cbecbb4a8074102011c8e36ab457407195aefcfb
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-10-25 00:38:43 +0530

    Refactor + better the camera

---
+++ src/MysteryIsland/Extensions/OrthographicCameraExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+
+namespace MysteryIsland.Extensions
+{
+    public static class OrthographicCameraExtensions
+    {
+        public static void LookAt(this OrthographicCamera camera, TiledMap map, PlayableCharacter character, Rectangle viewport)
+        {
+            var position = new Vector2(
+                    x: MathHelper.Clamp(character.Position.X, viewport.Width / 2, map.WidthInPixels - viewport.Width / 2),
+                    y: MathHelper.Clamp(character.Position.Y, viewport.Height / 2, map.HeightInPixels - viewport.Height / 2)
+                );
+            camera.LookAt(position);
+        }
+    }
+}
---
+++ src/MysteryIsland/PlayableCharacter.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+
+namespace MysteryIsland
+{
+    public class PlayableCharacter
+    {
+        private AnimatedSprite sprite;
+        private Vector2 _position = new Vector2(100, 100);
+        public Vector2 Position => _position;
+
+
+
+        public void LoadContent(ContentManager content)
+        {
+            var spriteSheet = content.Load<SpriteSheet>("characters/char1-sprite.sf", new JsonContentLoader());
+            sprite = new AnimatedSprite(spriteSheet);
+            sprite.Play("walk-backward");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var walkSpeed = deltaSeconds * 128;
+            var keyboardState = Keyboard.GetState();
+            var animation = "look-backward";
+
+            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+            {
+                animation = "walk-forward";
+                _position.Y -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+            {
+                animation = "walk-backward";
+                _position.Y += walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+            {
+                animation = "walk-left";
+                _position.X -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+            {
+                animation = "walk-right";
+                _position.X += walkSpeed;
+            }
+
+            sprite.Play(animation);
+            sprite.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, Position);
+        }
+    }
+}
