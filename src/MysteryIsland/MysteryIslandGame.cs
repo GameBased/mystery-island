445000ae07c1a6de6b2598cdf66d4e9630325360
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-10-25 00:38:43 +0530

    Refactor + better the camera

--- src/MysteryIsland/MysteryIslandGame.cs
+++ src/MysteryIsland/MysteryIslandGame.cs
@@ -9,27 +9,27 @@ using MonoGame.Extended.Tiled.Renderers;
 using MonoGame.Extended;
 // Then you need a viewport adapter
 using MonoGame.Extended.ViewportAdapters;
-using System.Collections.Generic;
 using MonoGame.Extended.TextureAtlases;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Serialization;
 using MonoGame.Extended.Content;
+using MysteryIsland.Extensions;
 
 namespace MysteryIsland
 {
-    public class Game1 : Game
+    public class MysteryIslandGame : Game
     {
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private Vector2 _characterPosition;
-        private AnimatedSprite characterSprite;
+        public SpriteBatch SpriteBatch { get; private set; }
 
         // The camera object
         private OrthographicCamera camera;
         private TiledMap map;
         private TiledMapRenderer mapRenderer;
 
-        public Game1()
+        private PlayableCharacter character = new PlayableCharacter();
+
+        public MysteryIslandGame()
         {
             _graphics = new GraphicsDeviceManager(this);
 
@@ -52,7 +52,7 @@ namespace MysteryIsland
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Load the map
             map = Content.Load<TiledMap>("maps/exp");
@@ -62,55 +62,20 @@ namespace MysteryIsland
             var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 1280, 832);
             camera = new OrthographicCamera(viewportadapter);
 
-            var spriteSheet = Content.Load<SpriteSheet>("characters/char1-sprite.sf", new JsonContentLoader());
-            characterSprite = new AnimatedSprite(spriteSheet);
-            characterSprite.Play("walk-backward");
 
-            _characterPosition = new Vector2(100, 100);
+            character.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
 
             // TODO: Add your update logic here
-
-            var deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            var walkSpeed = deltaSeconds * 128;
-            var keyboardState = Keyboard.GetState();
-            var animation = "look-backward";
-
-            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
-            {
-                animation = "walk-forward";
-                _characterPosition.Y -= walkSpeed;
-            }
-
-            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
-            {
-                animation = "walk-backward";
-                _characterPosition.Y += walkSpeed;
-            }
-
-            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
-            {
-                animation = "walk-left";
-                _characterPosition.X -= walkSpeed;
-            }
-
-            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
-            {
-                animation = "walk-right";
-                _characterPosition.X += walkSpeed;
-            }
-
-            characterSprite.Play(animation);
-            characterSprite.Update(gameTime);
+            character.Update(gameTime);
+            
             // Update the map renderer
             mapRenderer.Update(gameTime);
-            // If you used the camera then we update it aswell
-            camera.LookAt(_characterPosition);
+            camera.LookAt(map, character, GraphicsDevice.Viewport.Bounds);
             
             base.Update(gameTime);
         }
@@ -119,21 +84,19 @@ namespace MysteryIsland
         {
             GraphicsDevice.Clear(Color.Black);
 
-
-
             // Transform matrix is only needed if you have a camera
             // Setting the sampler state to `new SamplerState { Filter = TextureFilter.Point }` will reduce gaps and odd artifacts when using animated tiles
-            _spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(), samplerState: new SamplerState { Filter = TextureFilter.Point });
+            SpriteBatch.Begin(transformMatrix: camera.GetViewMatrix(), samplerState: new SamplerState { Filter = TextureFilter.Point });
 
             // Then we will render the map
             mapRenderer.Draw(camera.GetViewMatrix());
 
-            _spriteBatch.Draw(characterSprite, _characterPosition);
+            character.Draw(SpriteBatch);
+
+            // base.Draw(gameTime);
 
             // End the sprite batch
-            _spriteBatch.End();
-
-            base.Draw(gameTime);
+            SpriteBatch.End();            
         }
     }
 }
--- src/MysteryIsland/Program.cs
+++ src/MysteryIsland/Program.cs
@@ -7,7 +7,7 @@ namespace MysteryIsland
         [STAThread]
         static void Main()
         {
-            using var game = new Game1();
+            using var game = new MysteryIslandGame();
             game.Run();
         }
     }
