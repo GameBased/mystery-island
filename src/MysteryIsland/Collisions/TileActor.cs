61ce9db957dea5179ae86c666c3c6bc5e6879d4c
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-10-26 00:00:03 +0530

    Setup Monogame.Extended collision detection
    Need more work tho... it collides before it should.
    Setting a breakpoint on PlayableCharacter.OnCollision shows useful info
    in the event args.
    
    It appears as if the character shouldn't even collide with the TiledActor at that point.

---
+++ src/MysteryIsland/Collisions/TileActor.cs
@@ -0,0 +1,28 @@
+﻿using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
+using MonoGame.Extended.Tiled;
+using System;
+
+namespace MysteryIsland.Collisions
+{
+    class TileActor : ICollisionActor
+    {
+        public TileActor(TiledMapTile tile, int tileWIdth, int tileHeight)
+        {
+            if (tile.IsBlank) throw new InvalidOperationException("Blank tiles cannot be checked for collision");
+            Bounds = new RectangleF(
+                     x: tile.X * tileWIdth,
+                     y: tile.Y * tileHeight,
+                 width: tileWIdth,
+                height: tileHeight
+            ); 
+        }
+
+        public IShapeF Bounds { get; set; }
+
+        public virtual void OnCollision(CollisionEventArgs collisionInfo)
+        {
+
+        }
+    }
+}
