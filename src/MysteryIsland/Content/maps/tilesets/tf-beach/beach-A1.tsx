34b0ad00b56db68d4e78cc9b60a8900a398ff2a6
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-11-14 22:25:19 +0530

    Add a beach map

---
+++ src/MysteryIsland/Content/maps/tilesets/tf-beach/beach-A1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="beach-A1" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="tf_beach_tileA1.png" width="512" height="384"/>
+</tileset>
